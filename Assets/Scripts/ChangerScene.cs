9480ffa567e8ce2ac90a9560030570520a05c22b
darwinn43 <darwinn43@hotmail.com>
2025-03-24 09:47:53 -0400

    Reajuster la taille de menu
    J'ai réajusté la taille des buttons et du logo et j,ai ajouter un option quitter

--- Assets/Scripts/ChangerScene.cs
+++ Assets/Scripts/ChangerScene.cs
@@ -15,6 +15,8 @@ public class BoutonScript : MonoBehaviour
     }
     public void quitter()
     {
+        Application.Quit();
+        UnityEditor.EditorApplication.isPlaying = false;
     }
 
     
