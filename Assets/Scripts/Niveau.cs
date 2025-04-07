368c51e5b5b82218533daad1669463be43e029dc
coolmonkeyguydude <yass.att1234@gmail.com>
2025-04-07 11:57:45 -0400

    Interface quadrillé (4)
    Ajout de la possibilité de changer la taille des carrés dans le quadrillage et début de l'adaptation de cette taille à la taille de la caméra de la scène avec un script

--- Assets/Scripts/Niveau.cs
+++ Assets/Scripts/Niveau.cs
@@ -1,40 +1,23 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Niveau : MonoBehaviour
 {
-    public Camera camera;
-    public GameObject grillePoints;
 
-    private const float distanceEntrePoints = 0.5f;
-    
+    public Material quadrillage;
 
     // Start is called before the first frame update
-    public void creerNiveau()
+    public void Start()
     {
         Debug.Log("creer niveau");
 
-        Point pointAncre1 = new Point(-30, -10, grillePoints.transform);
-        Point pointAncre2 = new Point(30, 10, grillePoints.transform);
+        quadrillage.SetFloat("_tiling", 5.0f);
 
-        var distanceEntrePointsAncres = pointAncre1.getCoordonneX() - pointAncre2.getCoordonneX();
 
-        var x = 0.0f;
 
-        while(x < distanceEntrePointsAncres)
-        {
 
-            var y = -camera.orthographicSize;
-
-            while(y < Math.Abs(y))
-            {
-                new Point(x, y, grillePoints.transform);
-                y += distanceEntrePoints;
-            }
-
-            x += distanceEntrePointsAncres; 
-        }
-        
     }
 
     // Update is called once per frame
