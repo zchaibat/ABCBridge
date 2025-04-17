f4de08e122a5ffd246461212f28c4c36fafdb2cd
coolmonkeyguydude <yass.att1234@gmail.com>
2025-04-17 11:59:23 -0400

    Fonctionnement du jeu (1)
    Développement du script qui gère la logique du niveau et liaison du script Niveau à l'interface du jeu

--- Assets/Scripts/Niveau.cs
+++ Assets/Scripts/Niveau.cs
@@ -1,19 +1,28 @@
 using System;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class Niveau : MonoBehaviour
 {
-
+    public float budget;
+    public GameObject texteBudget;
     public Material quadrillage;
+    public Camera camera;
+
+    private Boolean modeConstruction = true;
+    private Boolean modeDeboguage = false;
 
     // Start is called before the first frame update
     public void Start()
     {
-        Debug.Log("creer niveau");
 
-        quadrillage.SetFloat("_tiling", 5.0f);
+        Double tailleCarre = ((Double) (6/20))*camera.orthographicSize;
+
+        quadrillage.SetFloat("_carresParMetres", (float) tailleCarre);
+        texteBudget.GetComponent<TextMeshProUGUI>().SetText(budget.ToString() + "$");
 
 
 
