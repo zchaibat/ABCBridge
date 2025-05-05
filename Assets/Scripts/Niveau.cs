93502f069b7c0be0170b39eca7026433cf00c2a9
coolmonkeyguydude <yass.att1234@gmail.com>
2025-05-05 12:03:21 -0400

    Fonctionnement du jeu (1)
    Codage de la logique du jeu (Script niveau)

--- Assets/Scripts/Niveau.cs
+++ Assets/Scripts/Niveau.cs
@@ -7,30 +7,38 @@ using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class Niveau : MonoBehaviour
 {
-    public float budget;
     public GameObject texteBudget;
     public Material quadrillage;
     public Camera camera;
-
     private Boolean modeConstruction = true;
     private Boolean modeDeboguage = false;
+    public GameObject referencePoint;
 
-    
+    public GameObject vehicule;
+    public GameObject mapGauche;
+    public GameObject mapDroite;
+    public float budget;
+   
+
+
     public void Start()
     {
+        Time.timeScale = 0;
 
         float tailleCarre = (float) (15 / camera.orthographicSize);
 
-        print(tailleCarre);
-
-        float tailleCercle = (float) (1 / tailleCarre);
+        float tailleCercle = 1/tailleCarre;
+        referencePoint.transform.localScale = new Vector3(tailleCercle, tailleCercle, tailleCercle);
 
         quadrillage.SetFloat("_carresParMetres",  tailleCarre);
         texteBudget.GetComponent<TextMeshProUGUI>().SetText(budget.ToString() + "$");
 
+    }
 
-
-
+    public void commencerPartie()
+    {
+        modeConstruction = false;
+        Time.timeScale = 1;
     }
 
     // Update is called once per frame
